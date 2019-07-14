fcb5cd9065894730a46f77d265016b4c6110eb6f
Mark McDaniels <mcdaniels.mark@gmail.com>
2019-07-14 15:24:25 -0400

    Escape
    I didn't account for special characters in the innerHTML, and remedied it.

--- public/js/hover-text.js
+++ public/js/hover-text.js
@@ -3,7 +3,13 @@
 *   Takes the inner html that is truncated in the calender, and places the text in the title attribute for hover reading.
 *
 */
-
+const escape = text => {
+    return text.replace(/&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&quot;/g, '"')
+    .replace(/&#039;/g, "'")
+}
 
 let elements = document.getElementsByClassName('fc-title');
 
@@ -11,5 +17,5 @@ let theKeys = Object.keys(elements);
 
 theKeys.forEach( key => {
     let text = elements[key].innerHTML;
-    elements[key].setAttribute('title', text);
+    elements[key].setAttribute('title', escape(text));
 });
